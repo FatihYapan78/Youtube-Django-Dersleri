1c33e8bc25ff1ee597121b21a8b7f3ffa52671c8
Fatih Yapan <yapanfatih78@gmail.com>
2023-07-17 16:56:38 +0300

    admin-customize

--- NewProje/game/Appgame/admin.py
+++ NewProje/game/Appgame/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Userinfo)
+
+
+@admin.register(Userinfo)
+class Userinfo(admin.ModelAdmin):
+
+    list_display = ('user','password')
+    list_filter = ('user',)
+    search_fields = ('user__username',)
+
